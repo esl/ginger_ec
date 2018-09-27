cb83f1793e4eb4c53aa8b79faa795033bf9a5539
Linus Schoemaker <linus@driebit.nl>
2018-09-27 15:54:22 +0200

    Fix syntax errors

--- modules/mod_ginger_base/controllers/controller_search.erl
+++ modules/mod_ginger_base/controllers/controller_search.erl
@@ -108,7 +108,7 @@ is_visible(Document, _Context) when is_map(Document) ->
 -spec search_result(m_rsc:resource() | map(), z:context()) -> map().
 search_result(Id, Context) when is_integer(Id) ->
     Rsc = m_ginger_rest:rsc(Id, Context),
-    m_ginger_rest:with_edges(Rsc, [depiction], Context),
+    m_ginger_rest:with_edges(Rsc, [depiction], Context);
 search_result(Document, _Context) when is_map(Document) ->
     %% Return a document (such as an Elasticsearch document) as is.
     Document.
